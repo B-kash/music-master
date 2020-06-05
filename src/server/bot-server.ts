5d9a3803adc7eb889016c542bd7aacc1bd777aa0
bikash_chapagain <bikash.chapagain@theoplayer.com>
2020-06-05 23:12:28 +0200

    ✨ music play feature POC completed

--- src/server/bot-server.ts
+++ src/server/bot-server.ts
@@ -1,8 +1,7 @@
 import Discord from 'discord.js';
-
+import ytdl from 'ytdl-core';
 export class BotServer {
     private bot: Discord.Client;
-
     constructor() {
         this.bot = new Discord.Client();
     }
@@ -17,16 +16,18 @@ export class BotServer {
             console.log('HEllo world i am here');
         });
         this.bot.on('message', (message: Discord.Message) => {
+            if (message.author.bot) {
+                return;
+            }
             if (message.content.substring(0, process.env.PREFIX.length) === process.env.PREFIX) {
                 const args = message.content.substring(process.env.PREFIX.length).split(" ");
                 const command = args.shift();
                 switch (command) {
-                    case 'ping':
-                        message.reply('Pong!');
-                        break;
                     case 'tic':
                         message.reply('TAC!');
                         break;
+                    case 'join':
+                        this.joinVoice(message, args.shift());
                     default:
                         message.channel.send('DAMNN!!');
                 }
@@ -34,5 +35,40 @@ export class BotServer {
                 console.log('Not mine!!');
             }
         });
+    }
+
+    private async joinVoice(message: Discord.Message, url: string) {
+        const voiceChannel = message.member.voice.channel;
+        if (!voiceChannel)
+            return message.channel.send(
+                "You need to be in a voice channel to play music!"
+            );
+        const permissions = voiceChannel.permissionsFor(message.client.user);
+        if (
+            !permissions.has("CONNECT")
+            || !permissions.has("SPEAK")
+        ) {
+            return message.channel.send(
+                "I need the permissions to join and speak in your voice channel!"
+            );
+        }
+        try {
+            let connection = await voiceChannel.join();
+            this.play(connection, url);
+        } catch (e) {
+            console.log('Cant join with error ', e);
+            return message.reply('Cant join with error ' + e);
+        }
+
+    }
+
+    private async play(connection: Discord.VoiceConnection, url: string) {
+
+        connection.play(await ytdl(url)).on("finish", () => {
+            console.log('finished playing')
+        }).on('error', () => {
+            console.log("Fuck you error come")
+        });
+
     }
 }
